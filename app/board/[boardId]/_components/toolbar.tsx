8d9f1f7ff6c46680cb62d0c47f523936e9bc682f
Ayush Chavan <chavanayushp@gmail.com>
2025-07-26 22:14:37 +0530

    completed the working of Toolbar component

--- app/board/[boardId]/_components/toolbar.tsx
+++ app/board/[boardId]/_components/toolbar.tsx
@@ -1,34 +1,67 @@
-export const Toolbar=()=>{
-    return (
-        <div className="absolute top-[50%] -translate-y-[50%] left-2 flex flex-col gap-y-4">
-             <div className="bg-white rounded-md p-1.5 flex gap-y-1 flex-col items-center shadow-md">
-                 <div>
-                    Pencil
-                 </div>
-                 <div>
-                    Square
-                 </div>
-                  <div>
-                    Circle
-                 </div>
-                  <div>
-                    Ellipses
-                 </div>
-             </div>
-             <div className="bg-white rounded-md p-1.5 flex gap-y-1 flex-col items-center shadow-md">
-                  <div>
-                     Undo
-                  </div>
-                  <div>
-                     Redo
-                  </div>
-             </div>
-        </div>
-    );
+import { Circle, MousePointer2, Pencil, Redo2, Square, StickyNote, Type, Undo2 } from "lucide-react";
+import { ToolButton } from "./tool-button";
+
+export const Toolbar = () => {
+   return (
+      <div className="absolute top-[50%] -translate-y-[50%] left-2 flex flex-col gap-y-4">
+         <div className="bg-white rounded-md p-1.5 flex gap-y-1 flex-col items-center shadow-md">
+            <ToolButton
+               label="Select"
+               icon={MousePointer2}
+               onClick={() => { }}
+               isActive={false}
+            />
+            <ToolButton
+               label="Text"
+               icon={MousePointer2}
+               onClick={() => { }}
+               isActive={false}
+            />
+            <ToolButton
+               label="Sticky note"
+               icon={StickyNote}
+               onClick={() => { }}
+               isActive={false}
+            />
+            <ToolButton
+               label="Rectangle"
+               icon={Square}
+               onClick={() => { }}
+               isActive={false}
+            />
+            <ToolButton
+               label="Ellipse"
+               icon={Circle}
+               onClick={() => { }}
+               isActive={false}
+            />
+            <ToolButton
+               label="Pen"
+               icon={Pencil}
+               onClick={() => { }}
+               isActive={false}
+            />
+         </div>
+         <div className="bg-white rounded-md p-1.5 flex gap-y-1 flex-col items-center shadow-md">
+            <ToolButton
+               label="Undo"
+               icon={Undo2}
+               onClick={() => { }}
+               isActive={true}
+            />
+            <ToolButton
+               label="Redo"
+               icon={Redo2}
+               onClick={() => { }}
+               isActive={true}
+            />
+         </div>
+      </div>
+   );
 };
 
 export function ToolbarSkeleton() {
    return (
-        <div className="absolute top-[50%] -translate-y-[50%] left-2 flex flex-col gap-y-4 shadow-md rounded-md bg-white h-[360px] w-[52px]" />
+      <div className="absolute top-[50%] -translate-y-[50%] left-2 flex flex-col gap-y-4 shadow-md rounded-md bg-white h-[360px] w-[52px]" />
    );
 };
